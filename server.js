d3886f4711933930caa1b110ad8c0bbe316d4ba3
grandmasternik <nikkimallory13@gmail.com>
2021-07-08 05:31:12 -0500

    front-end folder add

--- server.js
+++ server.js
@@ -1,12 +1,16 @@
+const path = require('path');
 const express = require("express");
-
 const PORT = process.env.PORT || 4000;
-
 const app = express();
 
+app.use(express.static(path.resolve(__dirname, '../chakra-zu/build')));
 
 app.get("/api", (req, res) => {
     res.json({ message: "Welcome to Chakra Zulu Server!" });s
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, '../chakra-zu/build', 'index.html'));
 });
 
 app.listen(PORT, () => {
