de1137f581fb137d9e53952b3b61b06b6c936774
grandmasternik <nikkimallory13@gmail.com>
2021-07-08 16:16:06 -0500

    seed and serverjs updated

---
+++ chakraSeed.js
@@ -0,0 +1,87 @@
+const Chakra = [
+    {
+        name: 'Sahasrara (Crown Chakra)' ,
+        located: 'Top of head',
+        color: 'Violet',
+        element: 'Light',
+        purpose: 'Unity and manifestation',
+        functions: 'Knowing',
+        focus: 'Meaning',
+        brings: 'Order and connection to source',
+        obstacle: 'Lack of belief'
+    },
+
+    {
+        name: 'Ajna (Third Eye Chakra)',
+        located: 'Centre of forehead',
+        color: 'Indigo',
+        element: 'Ether',
+        purpose: 'Command perception',
+        functions: 'Seeing and intuition',
+        focus: 'Imagination',
+        brings: 'Ability to create your own reality',
+        obstacle: 'Illusion'
+    },
+
+    {
+        name: 'Vishuddha (Throat Chakra)',
+        located: 'Base of throat',
+        color: 'Blue',
+        element: 'Sound',
+        purpose: 'Vibration and connection',
+        functions: 'Communication and creativity',
+        focus: 'Self-expression',
+        brings: 'Information, expansion and telepathy',
+        obstacle: 'Low awareness'
+    },
+
+    {
+        name: 'Anahata (Heart Chakra)',
+        located: 'Centre of chest',
+        color: 'Green',
+        element: 'Air',
+        purpose: 'Integration and balance',
+        functions: 'Love',
+        focus: 'Relationships',
+        brings: 'Empathy, compassion and love',
+        obstacle: 'Grief & Lack of forgiveness'
+    },
+
+    {
+        name: 'Manipura (Solar Plexus Chakra)',
+        located: 'Between the diaphragm and navel',
+        color: 'Yellow',
+        element: 'Fire',
+        purpose: 'Transformation',
+        functions: 'Willpower',
+        focus: 'Action',
+        brings: 'Self-esteem and joy',
+        obstacle: 'Shame'
+    },
+
+    {
+        name: 'Swadisthana (Sacral Chakra)',
+        located: 'Between the navel and pubic bone',
+        color: 'Orange',
+        element: 'Water',
+        purpose: 'Movement and change',
+        functions: 'Desire, pleasure and sexuality',
+        focus: 'Feelings',
+        brings: 'Emotions and creativity',
+        obstacle: 'Guilt'
+    },
+
+    {
+        name: 'Muladhara (Root Chakra)',
+        located: 'Base of the spine',
+        color: 'Red',
+        element: 'Earth',
+        purpose: 'Solid grounding',
+        functions: 'Survival',
+        focus: 'Body',
+        brings: 'Stability, security and stillness',
+        obstacle: 'Fear'
+    }
+]
+
+module.exports = Chakra
--- server.js
+++ server.js
@@ -1,17 +1,61 @@
+require("dotenv").config();
+// Dependencies//
+
 const path = require('path');
 const express = require("express");
-const PORT = process.env.PORT || 4000;
 const app = express();
 
+// Configure Mongoose //
+
+const mongoose = require("mongoose");
+const db = mongoose.connection;
+const methodOverride = require('method-override');
+const PORT = process.env.PORT || 4000;
+// const cors = require("cors");
+
+//  Seed //
+
+// const dataSeed = require('dataSeed.js');
+
+// get driver connection
+// const dbo = require("./db/conn");
+
+// Body Parser Middleware //
 app.use(express.static(path.resolve(__dirname, '../chakra-zu/build')));
+// app.use(cors());
+app.use(express.json());
+// app.use(require("./routes/record"));
+app.use(methodOverride(`_method`))
+
+//  Set Default 'view engine' //
+
+// app.set('view engine', 'ejs');
+
+// Routes //
 
 app.get("/api", (req, res) => {
-    res.json({ message: "Welcome to Chakra Zulu Server!" });s
+    res.json({ message: "Welcome to Prana Zulu Server!" });
 });
 
 app.get('*', (req, res) => {
     res.sendFile(path.resolve(__dirname, '../chakra-zu/build', 'index.html'));
 });
+
+//  Database //
+
+//Database 
+mongoose.connect(process.env.DATABASE_URL, {
+  useNewUrlParser: true,
+  useUnifiedTopology: true,
+  useCreateIndex: true,
+  useFindAndModify: false
+});
+
+db.on('connected', () => console.log('mongo connected'));
+db.on('error', (err) => console.log(err.message, ' is mongo connected?'));
+db.on('disconnected', () => console.log('mongo disconnected'));
+
+// Web Server/Listener //
 
 app.listen(PORT, () => {
   console.log(`Server listening on ${PORT}`);
